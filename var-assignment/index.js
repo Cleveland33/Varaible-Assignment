b175f1f13fe793728a0154d08671232b7d6a4e2e
Cleveland33 <77119527+Cleveland33@users.noreply.github.com>
2021-01-26 15:25:13 -0500

    Add files via upload

---
+++ var-assignment/index.js
@@ -0,0 +1,44 @@
+var myName = "Trey";
+var yourName = prompt("What is your name?")
+alert("My name is" + myName +", variable assignment" + yourName + "!");
+
+function test(){
+    var a = "3";
+    var b = "8";
+
+    var c = a;
+    a = b;
+    b = c;  
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
+
+//var my name = "trey"; CAN NOT HAVE SPACE IN VARIABLE NAME... THINKS ITS TWO DIFFERENT COMMANDS.
+//var 123 = 123; CAN NOT HAVE VARIABLE STARTING WITH NUMBER.
+
+var my357 = 357;
+console.log("my357");
+
+var abc123$_//only characters allowed to be used
+
+
+void main()
+{
+    int `i`;
+
+    printf("Numbers from 0 to 100 divisible by 2\n");
+
+    for(i = 0; i <= 100; i++)
+    {
+        if(i%4 == 0)
+        {
+            printf("%d\t", i);
+        }
+        i++;
+     }
+
+     getch();
+}
+
+  
